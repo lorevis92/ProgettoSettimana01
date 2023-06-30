b2497f3676a1af77a7a64f12b4f4bd325b967acb
lorevis92 <lorenzo.visconti@hotmail.it>
2023-06-30 13:03:14 +0200

    Sviluppo classe "Immagine" ed adattamento del main per verificare le sue funzionalità

--- src/Elements/Immagine.java
+++ src/Elements/Immagine.java
@@ -1,5 +1,48 @@
 package Elements;
 
-public class Immagine {
+public class Immagine extends Multimedia {
+	// definizione attributi
+	private String titolo;
+	private int luminosità = 3;
+
+	// costruttori
+	public Immagine(String titolo) {
+		this.titolo = titolo;
+	}
+
+	// metodi
+	public void show() {
+		String stringaLuminosità = "";
+		for (int j = 0; j < this.luminosità; j++) {
+			stringaLuminosità = stringaLuminosità + "*";
+		}
+		System.out.printf("Titolo immagine: %s \nLuminosità: %s \n\n-----------------------------------------\n\n",
+				this.titolo,
+				stringaLuminosità);
+	}
+
+	public void alzaLuminosità() {
+		if (luminosità < 5)
+			this.luminosità++;
+		else
+			System.out.println("La luminosità non può essere alzata ulteriormente");
+	}
+
+	public void abbassaLiminosità() {
+		if (luminosità > 0)
+			this.luminosità--;
+		else
+			System.out.println("La luminosità non può essere diminuita ulteriormente");
+	}
+
+	// setters e getters
+
+	public int getLuminosità() {
+		return luminosità;
+	}
+
+	public void setLuminosità(int luminosità) {
+		this.luminosità = luminosità;
+	}
 
 }
